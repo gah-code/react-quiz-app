b4e44c54c8fc003f0a52d3d05655094e07723824
Gilbert Haro <gilbertharocode@gmail.com>
2024-08-06 00:12:16 -0700

    loading temp question data from json fake api, and handling loading, Error and Ready status

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+// import './App.css';
 
-// import Loader from './Loader';
-// import Error from './Error';
-import './App.css';
+// Loading Questions
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Action unkonwn');
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     fetch('http://localhost:3001/questions')
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .then((err) => console.log('Error'));
-    // .then((res) => res.json())
-    // .then((data) => dispatch({ type: 'dataReceived', payload: data }))
-    // .catch((err) => dispatch({ type: 'dataFailed' }));
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      // .then((err) => console.log('Error'));
+      // .then((res) => res.json())
+      // .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
   }, []);
 
   return (
     <div className='App'>
       <Header />
-      <Main />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
